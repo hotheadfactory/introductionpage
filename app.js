ab7d65f00e2cd25ae1ae7bffed27eab037956640
Andy H. Jung <commaniakr@gmail.com>
2020-10-13 16:58:36 +0900

    chore: set port to 8080

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var bodyparser = require('body-parser');
+var express = require("express");
+var bodyparser = require("body-parser");
 var app = express();
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
-var mysql = require('mysql');
-var dbconfig = require('./config/dbconfig');
-var port = 80;
-var router = require('./router/main')(app);
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+var mysql = require("mysql");
+var dbconfig = require("./config/dbconfig");
+var port = 8080;
+var router = require("./router/main")(app);
 
-app.set('views', __dirname + '/views');
-app.use('/static', express.static('static'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-app.use(express.static('static'));
+app.set("views", __dirname + "/views");
+app.use("/static", express.static("static"));
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
 
-
-
-app.listen(port, function(){
-    console.log('Server Start, Port : ' + port);
+app.listen(port, function () {
+  console.log("Server Start, Port : " + port);
 });
